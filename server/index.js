37ca4c32046a3d69fb99d3b6df0012a16b5b6499
Maksim Shelest <shelest.ks@gmail.com>
2023-02-27 17:59:24 +0300

    models done test get

--- server/index.js
+++ server/index.js
@@ -2,23 +2,31 @@ import express from 'express';
 // для считывания с файла переменных окружения
 import dotenv from 'dotenv';
 import sequelize from './db.js';
+import models from './models/models.js';
+// для отправки запросов с браузера
+import cors from 'cors';
 
 const env = dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'все ок' });
+});
 
 const start = async () => {
-	try {
-	  await sequelize.authenticate();
-		await sequelize.sync()
-		console.log("Подключение к бд установлено");
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    console.log('Подключение к бд установлено');
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (error) {
     console.log(error);
   }
 };
 
-
-start()
+start();
--- server/models/models.js
+++ server/models/models.js
@@ -1,1 +1,110 @@
-import sequelize from './db.js';
+import sequelize from '../db.js';
+import { DataTypes } from 'sequelize';
+
+// имя модели - 'user'
+const User = sequelize.define('user', {
+  // тип - число, primaryKey - первичный ключ, и будет автоинкрементироваться, те послед. записи будут +1
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  // тип - строка, должен не повторяться у пользователей
+  email: { type: DataTypes.STRING, unique: true },
+  // тип - строка, может повторяться у пользователей
+  password: { type: DataTypes.STRING },
+  // по умолчанию будем делать пользователя просто юзером
+  role: { type: DataTypes.STRING, defaultValue: 'USER' },
+});
+
+const Basket = sequelize.define('basket', {
+  // тип - число, primaryKey - первичный ключ, и будет автоинкрементироваться, те послед. записи будут +1
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+const BasketDevice = sequelize.define('basket_device', {
+  // тип - число, primaryKey - первичный ключ, и будет автоинкрементироваться, те послед. записи будут +1
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+const Device = sequelize.define('device', {
+  // тип - число, primaryKey - первичный ключ, и будет автоинкрементироваться, те послед. записи будут +1
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  // allowNull false - не может быть равно нулю
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+  price: { type: DataTypes.INTEGER, allowNull: false },
+  rating: { type: DataTypes.INTEGER, defaultValue: 0 },
+  img: { type: DataTypes.STRING, allowNull: false },
+});
+
+const Type = sequelize.define('type', {
+  // тип - число, primaryKey - первичный ключ, и будет автоинкрементироваться, те послед. записи будут +1
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+});
+
+const Brand = sequelize.define('brand', {
+  // тип - число, primaryKey - первичный ключ, и будет автоинкрементироваться, те послед. записи будут +1
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+});
+
+const Rating = sequelize.define('rating', {
+  // тип - число, primaryKey - первичный ключ, и будет автоинкрементироваться, те послед. записи будут +1
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  rate: { type: DataTypes.INTEGER, allowNull: false },
+});
+
+const DeviceInfo = sequelize.define('device_info', {
+  // тип - число, primaryKey - первичный ключ, и будет автоинкрементироваться, те послед. записи будут +1
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  title: { type: DataTypes.STRING, allowNull: false },
+  description: { type: DataTypes.STRING, allowNull: false },
+});
+
+const TypeBrand = sequelize.define('type_brand', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+// Описание связей моделей
+// Все внешние ключи sequelize добавит сам при связывании
+
+// // одна запись девайса в бд имеет много записей с характеристиками
+// Device.hasMany(DeviceInfo)
+
+// // для DeviceInfo указываем что эта сущность пренадлежит Device
+// DeviceInfo.belongesTo(Device);
+
+// cсвязь один к одному
+User.hasOne(Basket);
+// корзина пренадлежит пользователю
+Basket.belongsTo(User);
+
+// пользователь может иметь несколько оценок
+User.hasMany(Rating);
+Rating.belongsTo(User);
+
+Basket.hasMany(BasketDevice);
+BasketDevice.belongsTo(Basket);
+
+Type.hasMany(Device);
+Device.belongsTo(Type);
+
+Brand.hasMany(Device);
+Device.belongsTo(Brand);
+
+Device.hasMany(BasketDevice);
+BasketDevice.belongsTo(Device);
+
+// связь многие ко многим
+// необходимо указать связующую таблицу - связующую модель
+Type.belongsToMany(Brand, { through: TypeBrand });
+Brand.belongsToMany(Type, { through: TypeBrand });
+
+export default {
+  User,
+  Basket,
+  BasketDevice,
+  Brand,
+  Device,
+  DeviceInfo,
+  Rating,
+  Type,
+  TypeBrand,
+};
