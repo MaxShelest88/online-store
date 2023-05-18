cdfd9e9db02e85c09931d1622a55cbf29302b2cb
Maksim Shelest <shelest.ks@gmail.com>
2023-05-18 13:25:29 +0300

    auth page finish

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -5,9 +5,12 @@ import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Link } from 'react-router-dom';
-import { REGISTRATION_ROUTE } from '../utils/consts';
+import { useLocation } from 'react-router-dom';
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts';
 
 const Auth = () => {
+  let location = useLocation();
+  const isLogin = location.pathname === LOGIN_ROUTE;
   return (
     <Container
       className="d-flex justify-content-center align-items-center"
@@ -17,7 +20,7 @@ const Auth = () => {
         style={{ width: 600 }}
         className="p-5"
       >
-        <h2 className="m-auto">Авторизация</h2>
+        <h2 className="m-auto">{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
         <Form className="d-flex flex-column">
           <Form.Control
             className="mt-3"
@@ -29,16 +32,22 @@ const Auth = () => {
           />
           <Row>
             <Col>
-              <div>
-                Нет аккаунта? <Link to={REGISTRATION_ROUTE}>Зарегистрируйтесь</Link>
-              </div>
+              {isLogin ? (
+                <div>
+                  Нет аккаунта? <Link to={REGISTRATION_ROUTE}>Зарегистрируйтесь</Link>
+                </div>
+              ) : (
+                <div>
+                  Есть аккаунт? <Link to={LOGIN_ROUTE}>Авторизируйтесь</Link>
+                </div>
+              )}
             </Col>
             <Col className="col-12 col-sm-6">
               <Button
                 variant={'outline-success'}
                 className="align-self-end"
               >
-                Войти
+                {isLogin ? 'Войти' : 'Зарегистрироваться'}
               </Button>
             </Col>
           </Row>
